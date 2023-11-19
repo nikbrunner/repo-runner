1b7f36285639268f6a9d26690d19fef7724a3c43
nbr <nikolaus.brunner@protonmail.ch>
2023-11-19 13:46:36 +0100

    feat(add): initial working clone repo functionality

--- main.go
+++ main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 )
+
+type Config struct {
+	RepoPath  string `json:"repoPath"`
+	Separator string `json:"separator"`
+}
+
+func loadConfig() Config {
+	file, err := os.Open("config.json")
+	if err != nil {
+		fmt.Println("Error opening config file: ", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	config := Config{}
+	err = decoder.Decode(&config)
+
+	if err != nil {
+		fmt.Println("Error decoding config file: ", err)
+		os.Exit(1)
+	}
+
+	return config
+}
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: repo [--open|--add|--remove|--status|--help]")
+		printHelp()
 		return
 	}
 
@@ -15,7 +43,7 @@ func main() {
 	case "--open":
 		openRepo()
 	case "--add":
-		cloneRepo()
+		cloneRepo(os.Args[2])
 	case "--remove":
 		removeRepo()
 	case "--status":
@@ -27,8 +55,75 @@ func main() {
 	}
 }
 
-func cloneRepo() {
-	fmt.Println("Cloning repository...")
+func expandPath(path string) string {
+	if strings.Contains(path, "$HOME") {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			printNegative("Error getting home directory:", &err)
+			return ""
+		}
+		return strings.Replace(path, "$HOME", home, 1)
+	}
+	return path
+}
+
+func parseGitUrl(gitUrl string) (string, string, error) {
+	gitUrl = strings.TrimSuffix(gitUrl, ".git")
+
+	var parts []string
+
+	if strings.Contains(gitUrl, "git@") { // SSH format
+		parts = strings.Split(gitUrl, ":")
+		if len(parts) < 2 {
+			return "", "", fmt.Errorf("invalid Git Url")
+		}
+
+		parts = strings.Split(parts[1], "/")
+	} else { // Assume HTTP/HTTPS format
+		parts = strings.Split(gitUrl, "/")
+	}
+
+	if len(parts) < 2 {
+		return "", "", fmt.Errorf("invalid Git Url")
+	}
+
+	username := parts[len(parts)-2]
+	repoName := parts[len(parts)-1]
+
+	return username, repoName, nil
+}
+
+func cloneRepo(gitUrl string) {
+	config := loadConfig()
+
+	config.RepoPath = expandPath(config.RepoPath)
+	if config.RepoPath == "" {
+		return
+	}
+
+	separator := config.Separator
+	if separator == "" {
+		separator = "@"
+	}
+
+	// Parsing the Git URL to get username and repo name
+	username, repoName, err := parseGitUrl(gitUrl)
+	if err != nil {
+		printNegative("Error parsing Git URL", &err)
+	}
+
+	fullPath := fmt.Sprintf("%s/%s%s%s", config.RepoPath, username, separator, repoName)
+
+	printPositive("Cloning repostitory to " + fullPath)
+
+	cmd := exec.Command("git", "clone", gitUrl, fullPath)
+	err = cmd.Run()
+	if err != nil {
+		printNegative("Error cloning repository:", &err)
+		return
+	}
+
+	printPositive("Repository cloned successfully")
 }
 
 func removeRepo() {
@@ -63,6 +158,9 @@ func printInfo(message string) {
 	fmt.Printf("%s%s%s\n", colorBlue, message, colorReset)
 }
 
-func printNegative(message string) {
+func printNegative(message string, err *error) {
 	fmt.Printf("%s%s%s\n", colorRed, message, colorReset)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
