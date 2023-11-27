90eb3bda42dfe552974c24a30c27695b669df3f4
nbr <nikolaus.brunner@protonmail.ch>
2023-11-27 18:02:43 +0100

    fix(layout/default): use correct session path for all windows

--- layouts/default.sh
+++ layouts/default.sh
@@ -9,22 +9,25 @@ tmux new-session -d -s "$sessionName" -c "$sessionPath"
 # Rename the first window to "code"
 tmux rename-window -t "${sessionName}:1" "code"
 
-# Create a second window
-tmux new-window -t "$sessionName"
+# Run "nvim ." in the first window
+tmux send-keys -t "${sessionName}:1" "nvim ." Enter
+
+# Create a second window in the same path
+tmux new-window -t "$sessionName" -c "$sessionPath"
 
 # Rename the second window to "run"
 tmux rename-window -t "${sessionName}:2" "run"
 
 # Split the window into two equal vertical panes
-tmux split-window -h -t "${sessionName}:2"
+tmux split-window -h -t "${sessionName}:2" -c "$sessionPath"
 
 # Select the first pane (now pane 1) and split it into two horizontal panes
 tmux select-pane -t "${sessionName}:2.1"
-tmux split-window -v -t "${sessionName}:2"
+tmux split-window -v -t "${sessionName}:2" -c "$sessionPath"
 
 # Select the second pane (originally, now pane 2) and split it into two horizontal panes
 tmux select-pane -t "${sessionName}:2.2"
-tmux split-window -v -t "${sessionName}:2"
+tmux split-window -v -t "${sessionName}:2" -c "$sessionPath"
 
 # Distribute all panes evenly
 tmux select-layout -t "${sessionName}:2" tiled
